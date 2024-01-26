61acc44abf9775061a8885bc8af3acd87e912856
Kai Hatje <kai.hatje@app-akademie.com>
2024-01-26 16:49:36 +0100

    Made it even worse 😈

--- lib/MAIN.dart
+++ lib/MAIN.dart
@@ -25,30 +25,45 @@ class BlgScr extends StatefulWidget {
   const BlgScr({super.key});
 
   @override
-  _StateForBlgScren createState() => _StateForBlgScren();
+  State<BlgScr> createState() => _StateForBlgScren();
 }
 
 class _StateForBlgScren extends State<BlgScr> {
+  addCommentFunction(int postId, String comment) async {
+    var urlToAddComment = 'https://jsonplaceholder.typicode.com/comments';
+    var res = await http.post(
+      Uri.parse(urlToAddComment),
+      headers: {"Content-Type": "application/json"},
+      body: json.encode({"postId": postId, "body": comment}),
+    );
+    if (res.statusCode == 201) {
+      var urlAPI = 'https://jsonplaceholder.typicode.com/posts';
+      var responseFromServer = await http.get(Uri.parse(urlAPI));
+      if (responseFromServer.statusCode == 200) {
+        var dataFromServer = json.decode(responseFromServer.body);
+        setState(() {
+          blogs = dataFromServer;
+          doo = false;
+        });
+      } else {
+        setState(() {
+          blogs = [];
+          doo = false;
+        });
+      }
+    }
+  }
+
   List<dynamic> blogs = [];
   bool doo = true;
   bool isUpdating = false;
   final TextEditingController controller1 = TextEditingController();
   final TextEditingController controller4 = TextEditingController();
   final TextEditingController controller6 = TextEditingController();
-  int toUpdate = -1;
-  int slctPost = -1;
-  final TextEditingController ctrl2 = TextEditingController();
-  final TextEditingController ctrl3 = TextEditingController();
-
-  @override
-  void initState() {
-    super.initState();
-    ftch();
-  }
 
   ftch() async {
-    var urlAPI = 'https://jsonplaceholder.typicode.com/posts';
-    var responseFromServer = await http.get(Uri.parse(urlAPI));
+    var urlApi = 'https://jsonplaceholder.typicode.com/posts';
+    var responseFromServer = await http.get(Uri.parse(urlApi));
     if (responseFromServer.statusCode == 200) {
       var dataFromServer = json.decode(responseFromServer.body);
       setState(() {
@@ -61,6 +76,12 @@ class _StateForBlgScren extends State<BlgScr> {
         doo = false;
       });
     }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    ftch();
   }
 
   add_post_function(String titleParam, String bodyParam) async {
@@ -88,13 +109,8 @@ class _StateForBlgScren extends State<BlgScr> {
     }
   }
 
-  DeletePostFunction(int idParam) async {
-    var urlToDeletePost = 'https://jsonplaceholder.typicode.com/posts/$idParam';
-    var r = await http.delete(Uri.parse(urlToDeletePost));
-    if (r.statusCode == 200) {
-      ftch();
-    }
-  }
+  int toUpdate = -1;
+  int slctPost = -1;
 
   startUpdatePost_FUNCTION(
       int idParam, String currentTitle, String currentBody) {
@@ -105,6 +121,8 @@ class _StateForBlgScren extends State<BlgScr> {
       isUpdating = true;
     });
   }
+
+  final TextEditingController ctrl2 = TextEditingController();
 
   UPDATE_POST_FUNCTION(int idParam, String titleParam, String bodyParam) async {
     var urlToUpdatePost = 'https://jsonplaceholder.typicode.com/posts/$idParam';
@@ -136,30 +154,7 @@ class _StateForBlgScren extends State<BlgScr> {
     }
   }
 
-  addCommentFunction(int postId, String comment) async {
-    var urlToAddComment = 'https://jsonplaceholder.typicode.com/comments';
-    var res = await http.post(
-      Uri.parse(urlToAddComment),
-      headers: {"Content-Type": "application/json"},
-      body: json.encode({"postId": postId, "body": comment}),
-    );
-    if (res.statusCode == 201) {
-      var urlAPI = 'https://jsonplaceholder.typicode.com/posts';
-      var responseFromServer = await http.get(Uri.parse(urlAPI));
-      if (responseFromServer.statusCode == 200) {
-        var dataFromServer = json.decode(responseFromServer.body);
-        setState(() {
-          blogs = dataFromServer;
-          doo = false;
-        });
-      } else {
-        setState(() {
-          blogs = [];
-          doo = false;
-        });
-      }
-    }
-  }
+  final TextEditingController ctrl3 = TextEditingController();
 
   Widget buildBlogPost(dynamic postParam) {
     return InkWell(
@@ -271,6 +266,10 @@ class _StateForBlgScren extends State<BlgScr> {
   }
 
   Widget buildInputFormFunction() {
+    return buildInputFormFunctionInner();
+  }
+
+  Container buildContainer() {
     return Container(
       decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
       child: Padding(
@@ -303,6 +302,10 @@ class _StateForBlgScren extends State<BlgScr> {
         ),
       ),
     );
+  }
+
+  Container buildInputFormFunctionInner() {
+    return buildContainer();
   }
 
   Widget buildCommentFormFunction() {
@@ -447,6 +450,14 @@ class _StateForBlgScren extends State<BlgScr> {
             ),
     );
   }
+
+  DeletePostFunction(int idParam) async {
+    var urlToDeletePost = 'https://jsonplaceholder.typicode.com/posts/$idParam';
+    var r = await http.delete(Uri.parse(urlToDeletePost));
+    if (r.statusCode == 200) {
+      ftch();
+    }
+  }
 }
 
 class ScrPstDtl extends StatefulWidget {
@@ -454,38 +465,10 @@ class ScrPstDtl extends StatefulWidget {
   const ScrPstDtl({super.key, this.post});
 
   @override
-  _ScrPstDtlState createState() => _ScrPstDtlState();
+  State<ScrPstDtl> createState() => _ScrPstDtlState();
 }
 
 class _ScrPstDtlState extends State<ScrPstDtl> {
-  List<dynamic> comments = [];
-  bool isLoadingComments = true;
-  final TextEditingController c = TextEditingController();
-
-  @override
-  void initState() {
-    super.initState();
-    fetchComments();
-  }
-
-  fetchComments() async {
-    var urlAPI =
-        'https://jsonplaceholder.typicode.com/posts/${widget.post['id']}/comments';
-    var responseFromServer = await http.get(Uri.parse(urlAPI));
-    if (responseFromServer.statusCode == 200) {
-      var commentsData = json.decode(responseFromServer.body);
-      setState(() {
-        comments = commentsData;
-        isLoadingComments = false;
-      });
-    } else {
-      setState(() {
-        comments = [];
-        isLoadingComments = false;
-      });
-    }
-  }
-
   addNewComment(String commentText) async {
     var urlToAddComment = 'https://jsonplaceholder.typicode.com/comments';
     var responseForAddComment = await http.post(
@@ -496,13 +479,6 @@ class _ScrPstDtlState extends State<ScrPstDtl> {
     if (responseForAddComment.statusCode == 201) {
       fetchComments();
     }
-  }
-
-  Widget buildComment(dynamic comment) {
-    return ListTile(
-      title: Text(comment['name']),
-      subtitle: Text(comment['body']),
-    );
   }
 
   Widget buildCommentSection() {
@@ -521,8 +497,11 @@ class _ScrPstDtlState extends State<ScrPstDtl> {
                   shrinkWrap: true,
                   physics: const NeverScrollableScrollPhysics(),
                   itemCount: comments.length,
-                  itemBuilder: (context, index) {
-                    return buildComment(comments[index]);
+                  itemBuilder: (context, i) {
+                    return MyWidget(
+                      name: comments[i]['name'],
+                      body: comments[i]['body'],
+                    );
                   },
                 ),
           TextField(
@@ -578,8 +557,11 @@ class _ScrPstDtlState extends State<ScrPstDtl> {
                           shrinkWrap: true,
                           physics: const NeverScrollableScrollPhysics(),
                           itemCount: comments.length,
-                          itemBuilder: (context, index) {
-                            return buildComment(comments[index]);
+                          itemBuilder: (context, idx) {
+                            return MyWidget(
+                              name: comments[idx]['name'],
+                              body: comments[idx]['body'],
+                            );
                           },
                         ),
                   TextField(
@@ -609,6 +591,53 @@ class _ScrPstDtlState extends State<ScrPstDtl> {
           ],
         ),
       ),
+    );
+  }
+
+  fetchComments() async {
+    var urlAPI =
+        'https://jsonplaceholder.typicode.com/posts/${widget.post['id']}/comments';
+    var responseFromServer = await http.get(Uri.parse(urlAPI));
+    if (responseFromServer.statusCode == 200) {
+      var commentsData = json.decode(responseFromServer.body);
+      setState(() {
+        comments = commentsData;
+        isLoadingComments = false;
+      });
+    } else {
+      setState(() {
+        comments = [];
+        isLoadingComments = false;
+      });
+    }
+  }
+
+  List<dynamic> comments = [];
+  bool isLoadingComments = true;
+  final TextEditingController c = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    fetchComments();
+  }
+}
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key, required this.name, required this.body});
+  final String name;
+  final String body;
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(widget.name),
+      subtitle: Text(widget.body),
     );
   }
 }
